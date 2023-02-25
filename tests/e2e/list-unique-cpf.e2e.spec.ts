0b5f6d4cc2a19bdde24e85b7db94fd91cd157d52
Arthur Akira <arthurakira.dev@gmail.com>
2023-02-25 16:27:16 -0300

    test: add 'list unique cpf' e2e test

---
+++ tests/e2e/list-unique-cpf.e2e.spec.ts
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import supertest from "supertest";
+import httpStatus from "http-status";
+
+import { ExpressApp } from "@infra/http/app";
+import { prisma } from "@infra/data/prisma/connection";
+import { CPFHelper } from "@tests/helpers/cpf-helper";
+
+describe("[E2E] GET /cpf/:cpf", () => {
+  const server = supertest(new ExpressApp().app);
+  const existingCPF = "17854840016";
+
+  beforeEach(async () => {
+    await prisma.cleanDb();
+
+    await CPFHelper.insertCPF(existingCPF);
+  });
+
+  describe("Success", () => {
+    it("[200::OK] Should be able to list the CPF", async () => {
+      const response = await server.get(`/cpf/${existingCPF}`);
+
+      expect(response.statusCode).toEqual(httpStatus.OK);
+      expect(response.body).toHaveProperty("cpf", existingCPF);
+      expect(response.body).toHaveProperty("createdAt", expect.any(String));
+    });
+  });
+
+  describe("Failure", () => {
+    it("[404::NOT_FOUND] Should return an error if the CPF is not in the shortlist", async () => {
+      const fakeCPF = "23661589717";
+
+      const response = await server.get(`/cpf/${fakeCPF}`);
+
+      expect(response.statusCode).toEqual(httpStatus.NOT_FOUND);
+      expect(response.body).toHaveProperty("type", "NotFoundCpfException");
+      expect(response.body).toHaveProperty("message", "CPF not found");
+    });
+
+    it("[422::UNPROCESSABLE_ENTITY] Should return an error if the cpf has an invalid format", async () => {
+      const invalid = "23661589717invalid";
+
+      const response = await server.get(`/cpf/${invalid}`);
+
+      expect(response.statusCode).toEqual(httpStatus.UNPROCESSABLE_ENTITY);
+      expect(response.body).toHaveProperty("type", "InvalidCpfException");
+      expect(response.body).toHaveProperty("message", "Invalid CPF format");
+    });
+
+    it("[422::UNPROCESSABLE_ENTITY] Should return an error if the cpf is invalid", async () => {
+      const invalid = "17854840010";
+
+      const response = await server.get(`/cpf/${invalid}`);
+
+      expect(response.statusCode).toEqual(httpStatus.UNPROCESSABLE_ENTITY);
+      expect(response.body).toHaveProperty("type", "InvalidCpfException");
+      expect(response.body).toHaveProperty("message", "CPF is not valid");
+    });
+  });
+});
